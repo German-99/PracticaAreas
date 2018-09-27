7ac2a2f6e0d15d1d22fb86732dc59c885acfcb1f
german adrian anguiano perez <adrian2009@live.com.mx>
2018-09-27 08:48:11 -0700

    Mejorado con 16 errores

--- PracticaAreas/MainWindow.xaml.cs
+++ PracticaAreas/MainWindow.xaml.cs
@@ -14,7 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PracticaAreas
+
 {
+    
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
@@ -25,28 +27,38 @@ namespace PracticaAreas
             InitializeComponent();
             
         }
-
+        
         private void BotonRectangulo_Click(object sender, RoutedEventArgs e)
         {
             int aux = int.Parse(baseRectangulo.Text);
             int aux2 = int.Parse(alturaRectangulo.Text);
-            int aux3 = aux * aux2;
+            float aux3 = aux * aux2;
             arearectangulo.Text = aux3.ToString();
         }
         
         private void BotonTriangulo_Click(object sender, RoutedEventArgs e)
         {
-            int aux = int.Parse(b)
+            int aux = int.Parse(baseTriangulo.Text);
+            int aux2 = int.Parse(alturaTriangulo.Text);
+            float aux3 = (aux * aux2) / 2;
+            areaTriangulo.Text = aux3.ToString();
         }
 
         private void BotonCirculo_Click(object sender, RoutedEventArgs e)
         {
+            int aux = int.Parse(Radio.Text);
+            double aux3 = (aux * aux) * 3.14 ;
+            areaCirculo.Text = aux3.ToString();
 
         }
 
         private void BotonTrapecio_Click(object sender, RoutedEventArgs e)
         {
-
+            int aux = int.Parse(Basemayor.Text);
+            int aux2 = int.Parse(Basemenor.Text);
+            int aux3 = int.Parse(alturatrapecio.Text);
+            double aux4 = ((aux + aux2) / 2) * aux3;
+            areaTrapecio.Text = aux4.ToString();
         }
     }
 }
